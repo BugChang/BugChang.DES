7fd5d544787dca627ca54c8c2cc6b69b22c5e85a
81069 <81069@BUGCHANG-PC>
2018-09-27 23:47:01 +0800

    基础信息导入

---
+++ BugChang.DES.Web.Mvc/Controllers/BasicImportController.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using OfficeOpenXml;
+
+// For more information on enabling MVC for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace BugChang.DES.Web.Mvc.Controllers
+{
+    public class BasicImportController : Controller
+    {
+
+        private readonly IHostingEnvironment _hostingEnvironment;
+
+        public BasicImportController(IHostingEnvironment hostingEnvironment)
+        {
+            _hostingEnvironment = hostingEnvironment;
+        }
+
+        // GET: /<controller>/
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+
+        [HttpPost]
+        public IActionResult Import(IFormFile excelfile)
+        {
+            string sWebRootFolder = _hostingEnvironment.WebRootPath;
+            string sFileName = $"{Guid.NewGuid()}.xlsx";
+            FileInfo file = new FileInfo(Path.Combine(sWebRootFolder, sFileName));
+            try
+            {
+                using (FileStream fs = new FileStream(file.ToString(), FileMode.Create))
+                {
+                    excelfile.CopyTo(fs);
+                    fs.Flush();
+                }
+                using (ExcelPackage package = new ExcelPackage(file))
+                {
+                    StringBuilder sb = new StringBuilder();
+                    ExcelWorksheet worksheet = package.Workbook.Worksheets[1];
+                    int rowCount = worksheet.Dimension.Rows;
+                    int ColCount = worksheet.Dimension.Columns;
+                    bool bHeaderRow = true;
+                    for (int row = 1; row <= rowCount; row++)
+                    {
+                        for (int col = 1; col <= ColCount; col++)
+                        {
+                            if (bHeaderRow)
+                            {
+                                sb.Append(worksheet.Cells[row, col].Value.ToString() + "\t");
+                            }
+                            else
+                            {
+                                sb.Append(worksheet.Cells[row, col].Value.ToString() + "\t");
+                            }
+                        }
+                        sb.Append(Environment.NewLine);
+                    }
+                    return Content(sb.ToString());
+                }
+            }
+            catch (Exception ex)
+            {
+                return Content(ex.Message);
+            }
+        }
+    }
+
+
+}
